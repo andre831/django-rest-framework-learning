a3e3211893e06a7ce9756ee21a7ff99602b57c21
André Santos <andredupmd@gmail>
2021-09-29 17:31:15 -0300

    Add set method http 'DELETE'

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
-from app.views import tenis_list
+from app.views import tenis_list, tenis_sett
 
 from django.urls import path
 
 
 urlpatterns = [
-    path('', tenis_list)
+    path('', tenis_list),
+    path('<int:pk>/', tenis_sett)
 ]
--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ from app.models import Tenis
 #imports rest_framework
 from rest_framework.decorators import  api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 #methods for acess in view
 @api_view(['GET', 'POST'])
@@ -26,7 +27,38 @@ def tenis_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-       
-        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
-    
 
+        #for error, response 'status'
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+#get details, changes and delete object
+@api_view(['GET', 'PUT', 'DELETE'])
+def tenis_sett(request, pk):
+
+    #get object in database
+    try:
+        tenis = Tenis.objects.get(pk=pk)
+    
+    #in case don't found object, return:
+    except Tenis.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    #get object
+    if request.method == 'GET':
+        serializer = TenisSerializer(tenis)
+        return Response(serializer.data)
+
+    # for changes, happen request > valid data > save
+    elif request.method == 'PUT':
+        serializer = TenisSerializer(tenis, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        #in error case, response 404
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    # for delete, return message successfully 
+    elif request.method == 'DELETE':
+        tenis.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)

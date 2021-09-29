74e7c90418dd641f61c2bb6e4852dfa92408f342
André Santos <andredupmd@gmail>
2021-09-29 10:57:45 -0300

    Add method http 'POST'

--- app/views.py
+++ app/views.py
@@ -11,9 +11,22 @@ from rest_framework.decorators import  api_view
 from rest_framework.response import Response
 
 #methods for acess in view
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def tenis_list(request):
-    tenis = Tenis.objects.all()
-    serializer = TenisSerializer(tenis, many = True)
-    return Response(serializer.data)
+
+    #return all products
+    while(request.method == 'GET'): 
+        tenis = Tenis.objects.all()
+        serializer = TenisSerializer(tenis, many = True)
+        return Response(serializer.data)
+
+    #for post, return the data and save
+    if request.method == 'POST':
+        serializer = TenisSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+       
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+    
 

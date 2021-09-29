a99bbf464187b77be38eb3130e349a505378cad2
André Santos <andredupmd@gmail>
2021-09-29 10:05:26 -0300

    Add set Tenis

--- app/serializers.py
+++ app/serializers.py
@@ -1,1 +1,13 @@
-from models import Tenis
+#import models
+from typing import ClassVar
+
+from django.db.models import fields
+from app.models import Tenis
+
+#imports rest_framework 
+from rest_framework import serializers
+
+class TenisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tenis
+        fields = ['id', 'nome', 'marca', 'cor', 'preco']
